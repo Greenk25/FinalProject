ec0e29832a6f894d3a5e782358ba5f1072c878e0
Greenk25 <kalkidandegif@gmail.com>
2025-04-02 07:38:17 -0700

    Viewset for Medicines

--- pharmacy_management/pharmacy/views.py
+++ pharmacy_management/pharmacy/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Medicine
+from .serializers import MedicineSerializer
 
-# Create your views here.
+class MedicineViewSet(viewsets.ModelViewSet):
+    queryset = Medicine.objects.all()
+    serializer_class = MedicineSerializer
